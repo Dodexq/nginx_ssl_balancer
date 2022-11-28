c0c0cfb6e0c2a625d0bd517f0e16f1342d497cab
Dodex <dodexpwnz@gmail.com>
2022-11-29 00:20:58 +0300

    tar www

--- data/ansible-install.sh
+++ data/ansible-install.sh
@@ -1,5 +1,5 @@
-sudo apt upgrade -y
-sudo apt install ansible -y
-sudo apt install sshpass -y
-yes | ssh-keygen -b 2048 -t rsa -f /home/vagrant/.ssh/host03 -q -N ""
+#sudo apt upgrade -y
+#sudo apt install ansible -y
+#sudo apt install sshpass -y
+sudo su vagrant && ssh-keygen '-b' '2048' '-t' 'rsa' '-f' '/home/vagrant/.ssh/host03' '-q' '-N' ''
 cp /home/vagrant/.ssh/host03.pub /vagrant/data/host03.pub
