e72656ba93e6a01274c4a27ed62334a175f8a550
Dodex <dodexpwnz@gmail.com>
2022-11-29 20:13:10 +0300

    done ssh ansible autoauth

--- data/ansible-install.sh
+++ data/ansible-install.sh
@@ -1,5 +1,6 @@
-#sudo apt upgrade -y
-#sudo apt install ansible -y
-#sudo apt install sshpass -y
-sudo su vagrant && ssh-keygen '-b' '2048' '-t' 'rsa' '-f' '/home/vagrant/.ssh/host03' '-q' '-N' ''
+sudo apt upgrade -y
+sudo apt install ansible -y
+sudo apt install sshpass -y
+ssh-keygen -t rsa -N '' -b 2048 -f /home/vagrant/.ssh/host03 -q -C vagrant@ansible-server
+sudo chown vagrant /home/vagrant/.ssh/host03
 cp /home/vagrant/.ssh/host03.pub /vagrant/data/host03.pub
