3b4b6b4c74afe021271d03afebb9c27b0a1b1ff7
Dodex <dodexpwnz@gmail.com>
2022-12-03 19:53:59 +0300

    ENABLE 80 PORT DOM.RU!

--- data/host03.sh
+++ data/host03.sh
@@ -1,6 +1,8 @@
 cat /vagrant/data/host03.pub | tee -a /home/vagrant/.ssh/authorized_keys
 rm /vagrant/data/host03.pub
 sudo apt install certbot python3-certbot-nginx -
-
+sudo cp /vagrant/data/etc/nginx/sites-available/dodextest.ddns.net /etc/nginx/sites-available/
+sudo ln -s /etc/nginx/sites-available/dodextest.ddns.net /etc/nginx/sites-enabled/dodextest.ddns.net
+sudo cp -rf /vagrant/data/etc/letsencrypt/* /etc/letsencrypt/
 ##nginx certbot
 #certbot --nginx -d dodextest.ddns.net --email dodexpwnz@gmail.com --http-01-port=81
