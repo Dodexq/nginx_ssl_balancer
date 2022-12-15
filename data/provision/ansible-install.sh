6258b440ce7a3c25ad5c7d8f732b3ed1f90e585c
Dodex <dodexpwnz@gmail.com>
2022-12-15 21:05:24 +0300

    15.12 clean up

--- data/provision/ansible-install.sh
+++ data/provision/ansible-install.sh
@@ -4,7 +4,3 @@ sudo apt install sshpass -y
 ssh-keygen -t rsa -N '' -b 2048 -f /home/vagrant/.ssh/host03 -q -C vagrant@ansible-server
 sudo chown vagrant /home/vagrant/.ssh/host03
 cp /home/vagrant/.ssh/host03.pub /vagrant/data/host03.pub
-
-
-
-certbot certonly --standalone -d dodextest.ddns.net --non-interactive --agree-tos --email dodexpwnz@gmail.com --http-01-port=81
--- data/provision/host03.sh
+++ data/provision/host03.sh
@@ -1,7 +1,3 @@
 sudo apt update -y
 cat /vagrant/data/host03.pub | tee -a /home/vagrant/.ssh/authorized_keys
 rm /vagrant/data/host03.pub
-
-
-#sudo cp /vagrant/data/etc/nginx/sites-available/dodextest.ddns.net /etc/nginx/sites-available/
-#sudo ln -s /etc/nginx/sites-available/dodextest.ddns.net /etc/nginx/sites-enabled/dodextest.ddns.net
